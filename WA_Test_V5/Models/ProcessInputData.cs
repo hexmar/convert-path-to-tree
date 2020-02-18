1b40dc427bb35ba82de79592cc65419beda2715f
Evgeny Vyazovtsev <evo118@outlook.com>
2020-02-18 23:49:04 +0300

    Try multi threaded

--- WA_Test_V5/Models/ProcessInputData.cs
+++ WA_Test_V5/Models/ProcessInputData.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using OfficeOpenXml;
 using WA_Test_V5.Interface.TreeView;
 
@@ -9,23 +11,92 @@ namespace WA_Test_V5.Models
 {
 	public class ProcessInputData
 	{
-		private ExcelPackage pack;
+		//private ExcelPackage pack;
+		private FileInfo fileInfo;
 
 		public ProcessInputData(string filePath)
 		{
-			FileInfo fInfo = new FileInfo(filePath);
-			if (fInfo.Exists != true) throw new Exception();
-			pack = new ExcelPackage(fInfo);
+			fileInfo = new FileInfo(filePath);
+			if (fileInfo.Exists != true) throw new Exception();
+			//pack = new ExcelPackage(fInfo);
 		}
 
 		public List<TreeViewElements> GetData()
 		{
-			var sheets = pack.Workbook.Worksheets;
-			var dataSheet = sheets.First();
-			var numberOfRows = dataSheet.Dimension.End.Row;
-			var numberOfCols = dataSheet.Dimension.End.Column;
-			ExcelRange Cells = dataSheet.Cells;
+			int numberOfRows;
+			using (var pack = new ExcelPackage(fileInfo))
+			{
+				var sheets = pack.Workbook.Worksheets;
+				var dataSheet = sheets.First();
+				numberOfRows = dataSheet.Dimension.End.Row;
+			}
 
+			var rootNode = new ConcurrentDictionary<string, object>();
+			var numberOfThreads = 2;
+			var parts = new List<Tuple<int, int>>();
+
+			{
+				var step = (numberOfRows - 1) / numberOfThreads;
+				var index = 2;
+				while (index < numberOfRows)
+				{
+					var start = index;
+					index += step;
+					var end = index;
+					if (index > numberOfRows || index + step / 2 > numberOfRows)
+					{
+						end = numberOfRows;
+					}
+
+					index += 1;
+
+					parts.Add(Tuple.Create(start, end));
+				}
+			}
+
+			parts.AsParallel().ForAll(part =>
+			{
+				using (var pack = new ExcelPackage(fileInfo))
+				{
+					var sheets = pack.Workbook.Worksheets;
+					var dataSheet = sheets.First();
+					var numberOfCols = dataSheet.Dimension.End.Column;
+					ExcelRange Cells = dataSheet.Cells;
+
+					for (var row = part.Item1; row <= part.Item2; row++)
+					{
+						var node = rootNode;
+						for (var col = 1; col < numberOfCols - 2; col++)
+						{
+							var cell = Cells[row, col];
+							var val = cell.Value;
+							var cellValue = val.ToString();
+
+							var nextNode = node.GetOrAdd(cellValue, CreateDicitionary);
+
+							node = (ConcurrentDictionary<string, object>)nextNode;
+						}
+
+						var lastCoordinate = Cells[row, numberOfCols - 2].Value.ToString();
+
+						var existingSet = node.GetOrAdd(lastCoordinate, CreateSet);
+
+						var set = (ConcurrentDictionary<ItemWithCid, byte>)existingSet;
+
+						int cid;
+						var validCid = int.TryParse(Cells[row, numberOfCols].Value.ToString(), out cid);
+						if (validCid)
+						{
+							var cidItem = new ItemWithCid(cid, Cells[row, numberOfCols - 1].Value.ToString());
+							set.GetOrAdd(cidItem, 1);
+						}
+					}
+				}
+			});
+
+
+			return new List<TreeViewElements>();
+			/*
 			var rootNode = new Dictionary<string, object>();
 			for (var row = 2; row <= numberOfRows; row++)
 			{
@@ -119,6 +190,17 @@ namespace WA_Test_V5.Models
 			nodes.AddRange(leaves);
 
 			return nodes;
+			*/
+		}
+
+		private static object CreateDicitionary(string key)
+		{
+			return new ConcurrentDictionary<string, object>();
+		}
+
+		private static object CreateSet(string key)
+		{
+			return new ConcurrentDictionary<ItemWithCid, byte>();
 		}
 	}
 }
