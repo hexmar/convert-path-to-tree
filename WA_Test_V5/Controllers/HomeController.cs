24ffd54dfbf6d95b1cf7d412ba87030cc51140f8
Evgeny Vyazovtsev <evo118@outlook.com>
2020-02-19 02:40:20 +0300

    Code style and node sorting

--- WA_Test_V5/Controllers/HomeController.cs
+++ WA_Test_V5/Controllers/HomeController.cs
@@ -53,40 +53,42 @@ namespace WA_Test_V5.Controllers
         }
         public JsonResult GetTreeViewNew()
         {
-            var parser = new ProcessInputData(Server.MapPath("~/Content/InitialData.xlsx"));
-            var _SampleTreeView = parser.GetData();
-            int nodeUnicID = 0;
-            var ParentsDic = new Dictionary<int, string>();//no
-            var ReadyList = new List<JsTree3Node>();
-            var mainNode = new JsTree3Node()
+            using (var parser = new ProcessInputData(Server.MapPath("~/Content/InitialData.xlsx")))
             {
-                id = "0",
-                text = "Портфель проектов",
-                state = new State(true, false, false),
-                children = new List<JsTree3Node>(),
-                data = 0
-            };
-            ReadyList.Add(mainNode);
-            ParentsDic.Add(nodeUnicID, "0");
-            nodeUnicID++;
-            foreach (var elem in _SampleTreeView)
-            {
-                var newNode = new JsTree3Node()
+                var _SampleTreeView = parser.GetData();
+                int nodeUnicID = 0;
+                var ParentsDic = new Dictionary<int, string>();//no
+                var ReadyList = new List<JsTree3Node>();
+                var mainNode = new JsTree3Node()
                 {
-                    id = elem.ID,
-                    text = elem.Name,
+                    id = "0",
+                    text = "Портфель проектов",
                     state = new State(true, false, false),
                     children = new List<JsTree3Node>(),
-                    data = elem.CID
+                    data = 0
                 };
-                if (ParentsDic.ContainsValue(elem.Parent_ID.ToString()))
-                    ReadyList[ParentsDic.FirstOrDefault(x => x.Value == elem.Parent_ID.ToString()).Key].children.Add(newNode);
-
-                ReadyList.Add(newNode);
-                ParentsDic.Add(nodeUnicID, elem.ID.ToString());
+                ReadyList.Add(mainNode);
+                ParentsDic.Add(nodeUnicID, "0");
                 nodeUnicID++;
+                foreach (var elem in _SampleTreeView)
+                {
+                    var newNode = new JsTree3Node()
+                    {
+                        id = elem.ID,
+                        text = elem.Name,
+                        state = new State(true, false, false),
+                        children = new List<JsTree3Node>(),
+                        data = elem.CID
+                    };
+                    if (ParentsDic.ContainsValue(elem.Parent_ID.ToString()))
+                        ReadyList[ParentsDic.FirstOrDefault(x => x.Value == elem.Parent_ID.ToString()).Key].children.Add(newNode);
+
+                    ReadyList.Add(newNode);
+                    ParentsDic.Add(nodeUnicID, elem.ID.ToString());
+                    nodeUnicID++;
+                }
+                return Json(mainNode, JsonRequestBehavior.AllowGet);
             }
-            return Json(mainNode, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- WA_Test_V5/Models/ProcessInputData.cs
+++ WA_Test_V5/Models/ProcessInputData.cs
@@ -7,66 +7,106 @@ using WA_Test_V5.Interface.TreeView;
 
 namespace WA_Test_V5.Models
 {
-	public class ProcessInputData
+	public class ProcessInputData : IDisposable
 	{
 		private ExcelPackage pack;
+		private int numberOfRows;
+		private int numberOfCols;
+		private ExcelRange cells;
+		private int idCounter;
 
 		public ProcessInputData(string filePath)
 		{
 			FileInfo fInfo = new FileInfo(filePath);
 			if (fInfo.Exists != true) throw new Exception();
 			pack = new ExcelPackage(fInfo);
+
+			var sheets = pack.Workbook.Worksheets;
+			var dataSheet = sheets.First();
+			numberOfRows = dataSheet.Dimension.End.Row;
+			numberOfCols = dataSheet.Dimension.End.Column;
+			cells = dataSheet.Cells;
+		}
+
+		public void Dispose()
+		{
+			pack.Dispose();
 		}
 
 		public List<TreeViewElements> GetData()
 		{
-			var sheets = pack.Workbook.Worksheets;
-			var dataSheet = sheets.First();
-			var numberOfRows = dataSheet.Dimension.End.Row;
-			var numberOfCols = dataSheet.Dimension.End.Column;
-			ExcelRange Cells = dataSheet.Cells;
+			var rootNode = GetTree();
 
+			var nodes = GetList(rootNode);
+
+			return nodes;
+		}
+
+		private Dictionary<string, object> GetTree()
+		{
 			var rootNode = new Dictionary<string, object>();
 			for (var row = 2; row <= numberOfRows; row++)
 			{
-				var node = rootNode;
-				for (var col = 1; col < numberOfCols - 2; col++)
-				{
-					var cellValue = Cells[row, col].Value.ToString();
-					object nextNode;
-					var nodeExists = node.TryGetValue(cellValue, out nextNode);
-					if (!nodeExists)
-					{
-						nextNode = new Dictionary<string, object>();
-						node.Add(cellValue, nextNode);
-					}
+				var node = GetNodeLocation(rootNode, row);
 
-					node = (Dictionary<string, object>)nextNode;
-				}
+				var set = GetLeaveSet(node, row);
 
-				var lastCoordinate = Cells[row, numberOfCols - 2].Value.ToString();
-				object existingSet;
-				var setExists = node.TryGetValue(lastCoordinate, out existingSet);
-				if (!setExists)
-				{
-					existingSet = new HashSet<ItemWithCid>(EqualityComparer<ItemWithCid>.Default);
-					node.Add(lastCoordinate, existingSet);
-				}
-
-				var set = (HashSet<ItemWithCid>)existingSet;
-
-				int cid;
-				var validCid = int.TryParse(Cells[row, numberOfCols].Value.ToString(), out cid);
-				if (validCid)
-				{
-					var cidItem = new ItemWithCid(cid, Cells[row, numberOfCols - 1].Value.ToString());
-					set.Add(cidItem);
-				}
+				AddNode(set, row);
 			}
 
+			return rootNode;
+		}
+
+		private Dictionary<string, object> GetNodeLocation(Dictionary<string, object> rootNode, int row)
+		{
+			var node = rootNode;
+			for (var col = 1; col < numberOfCols - 2; col++)
+			{
+				var cellValue = cells[row, col].Value.ToString();
+				object nextNode;
+				var nodeExists = node.TryGetValue(cellValue, out nextNode);
+				if (!nodeExists)
+				{
+					nextNode = new Dictionary<string, object>();
+					node.Add(cellValue, nextNode);
+				}
+
+				node = (Dictionary<string, object>)nextNode;
+			}
+
+			return node;
+		}
+
+		private HashSet<ItemWithCid> GetLeaveSet(Dictionary<string, object> node, int row)
+		{
+			var lastCoordinate = cells[row, numberOfCols - 2].Value.ToString();
+			object existingSet;
+			var setExists = node.TryGetValue(lastCoordinate, out existingSet);
+			if (!setExists)
+			{
+				existingSet = new HashSet<ItemWithCid>(EqualityComparer<ItemWithCid>.Default);
+				node.Add(lastCoordinate, existingSet);
+			}
+
+			return (HashSet<ItemWithCid>)existingSet;
+		}
+
+		private void AddNode(HashSet<ItemWithCid> set, int row)
+		{
+			int cid;
+			var validCid = int.TryParse(cells[row, numberOfCols].Value.ToString(), out cid);
+			if (validCid)
+			{
+				var cidItem = new ItemWithCid(cid, cells[row, numberOfCols - 1].Value.ToString());
+				set.Add(cidItem);
+			}
+		}
+
+		private List<TreeViewElements> GetList(Dictionary<string, object> rootNode)
+		{
 			var nodes = new List<TreeViewElements>();
 			var leaves = new List<TreeViewElements>();
-			var idCounter = 0;
+			idCounter = 0;
 
 			var queue = new Queue<KeyValuePair<string, Dictionary<string, object>>>();
 			queue.Enqueue(new KeyValuePair<string, Dictionary<string, object>>(
@@ -80,34 +120,21 @@ namespace WA_Test_V5.Models
 				var keys = pair.Value.Keys.OrderBy(key => key);
 				foreach (var key in keys)
 				{
-					var currentTreeElementId = (idCounter++).ToString();
-					var currentTreeElement = new TreeViewElements
-					{
-						ID = currentTreeElementId,
-						Name = key,
-						CID = -2,
-						Parent_ID = pair.Key,
-					};
+					var currentTreeElement = CreateTreeElement(key, pair.Key);
 					nodes.Add(currentTreeElement);
 
 					var nextNode = pair.Value[key];
 					if (nextNode is HashSet<ItemWithCid>)
 					{
 						var set = (HashSet<ItemWithCid>)nextNode;
-						var treeElements = set.Select(item => new TreeViewElements
-						{
-							Parent_ID = currentTreeElementId,
-							Name = item.Name,
-							CID = item.Cid
-						});
-						leaves.AddRange(treeElements);
+						AddLeavesToList(set, leaves, currentTreeElement.ID);
 
 						continue;
 					}
 
 					var dictionary = (Dictionary<string, object>)nextNode;
 					queue.Enqueue(new KeyValuePair<string, Dictionary<string, object>>(
-						currentTreeElementId,
+						currentTreeElement.ID,
 						dictionary));
 				}
 			}
@@ -115,10 +142,45 @@ namespace WA_Test_V5.Models
 			foreach (var element in leaves)
 			{
 				element.ID = (idCounter++).ToString();
+				nodes.Add(element);
 			}
-			nodes.AddRange(leaves);
 
 			return nodes;
+		}
+
+		private TreeViewElements CreateTreeElement(string name, string parentId)
+		{
+			var currentTreeElement = new TreeViewElements
+			{
+				ID = (idCounter++).ToString(),
+				Name = name,
+				CID = -2,
+				Parent_ID = parentId,
+			};
+
+			return currentTreeElement;
+		}
+
+		private void AddLeavesToList(HashSet<ItemWithCid> leaves, List<TreeViewElements> list, string parentId)
+		{
+			var treeElements = leaves.Select(item => new TreeViewElements
+			{
+				Parent_ID = parentId,
+				Name = item.Name,
+				CID = item.Cid
+			});
+			foreach (var element in treeElements)
+			{
+				var insertIndex = list.FindIndex(addedElement => string.Compare(addedElement.Name, element.Name) > 0);
+				if (insertIndex < 0)
+				{
+					list.Add(element);
+				}
+				else
+				{
+					list.Insert(insertIndex, element);
+				}
+			}
 		}
 	}
 }
